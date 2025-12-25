9abc350c0bfaed5f7bd9e122e7c21568af87662c
abdulboriy qoraxojayev <abdulboriyqoraxojayev@MacBook-Air-abdulboriy.local>
2025-12-25 20:38:15 +0500

    comentri

---
+++ style.js
@@ -0,0 +1,14 @@
+const hamburgBtn = document.getElementById("btn-hed");
+const navbar = document.getElementById("navbar");
+
+btn -
+  hed.addEventListener("click", () => {
+    navbar.classList.toggle("open");
+    btn - hed.classList.toggle("close-btn");
+  });
+
+// DOM document object model
+
+// snake case hamburg_btn
+// kebab case hamburg-btn
+// camel case hamburgBtn
